ce2289795db444cc3f79e61c444c7ecdb210361e
andresap <andresap@protonmail.com>
2023-10-16 10:20:44 +0200

    Add cors for frontend app

--- src/main/kotlin/com/aap/casitabackend/controllers/HouseholdsController.kt
+++ src/main/kotlin/com/aap/casitabackend/controllers/HouseholdsController.kt
@@ -2,36 +2,30 @@ package com.aap.casitabackend.controllers
 
 import com.aap.casitabackend.entities.Household
 import com.aap.casitabackend.repositories.HouseholdsRepository
-import org.springframework.http.HttpStatus
+import com.aap.casitabackend.services.HouseholdsService
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 import java.util.*
 
 @RestController
 @RequestMapping("/api/households")
-class HouseholdsController(private val householdsRepository: HouseholdsRepository) {
+@CrossOrigin(origins = ["http://localhost:3000"])
+class HouseholdsController(
+    private val householdsRepository: HouseholdsRepository,
+    private val householdsService: HouseholdsService
+) {
 
     @GetMapping
-    fun getHouseholds(): Iterable<Household> = householdsRepository.findAll()
+    fun getHouseholds(): List<Household> = householdsService.getAllHouseholds()
 
     @GetMapping("/{id}")
-    fun getHouseholdById(@PathVariable id: String): Optional<Household> = householdsRepository.findById(id)
+    fun getHouseholdById(@PathVariable id: String): Optional<Household> = householdsService.getHouseholdById(id)
 
     @PostMapping
-    fun createHousehold(@RequestBody household: Household): Household {
-        return householdsRepository.insert(household)
-    }
+    fun createHousehold(@RequestBody household: Household): Household = householdsService.saveHousehold(household)
 
     @PutMapping("/{id}")
-    fun putHousehold(@PathVariable id: String, @RequestBody household: Household): ResponseEntity<Household> {
-
-        val existingHousehold = householdsRepository.findById(id)
-
-        if (existingHousehold.isEmpty) {
-            return ResponseEntity(createHousehold(household), HttpStatus.CREATED)
-        } else {
-            return ResponseEntity(householdsRepository.save(household), HttpStatus.OK)
-        }
-    }
+    fun putHousehold(@PathVariable id: String, @RequestBody household: Household): ResponseEntity<Household> =
+        householdsService.updateHousehold(id, household)
 }
 
--- src/main/kotlin/com/aap/casitabackend/services/HouseholdsService.kt
+++ src/main/kotlin/com/aap/casitabackend/services/HouseholdsService.kt
@@ -4,7 +4,10 @@ import com.aap.casitabackend.entities.Household
 import com.aap.casitabackend.entities.HouseholdMember
 import com.aap.casitabackend.repositories.HouseholdsRepository
 import org.springframework.data.mongodb.core.MongoTemplate
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class HouseholdsService(
@@ -12,6 +15,18 @@ class HouseholdsService(
     private val householdsRepository: HouseholdsRepository
 ) {
     fun saveHousehold(household: Household) = householdsRepository.save(household)
+
+    fun getAllHouseholds(): List<Household> = householdsRepository.findAll()
+
+    fun getHouseholdById(householdId: String): Optional<Household> = householdsRepository.findById(householdId)
+
+    fun updateHousehold(householdId: String, household: Household): ResponseEntity<Household> {
+        val existingHousehold = householdsRepository.findById(householdId)
+        return ResponseEntity(
+            saveHousehold(household),
+            if (existingHousehold.isEmpty) HttpStatus.CREATED else HttpStatus.OK
+        )
+    }
 
     fun addHouseholdMember(householdId: String, householdMember: HouseholdMember) {
         val household = householdsRepository.findById(householdId).orElse(null)
