d2c62a390b6eb0c3da4b21b03bdd9389421378e1
Andrés Arango Pérez <a.arango@tu-braunschweig.de>
2023-11-07 09:49:39 +0100

    Fix warnings from code analysis

--- src/main/kotlin/com/aap/casitabackend/SecurityConfig.kt
+++ src/main/kotlin/com/aap/casitabackend/SecurityConfig.kt
@@ -3,8 +3,6 @@ package com.aap.casitabackend
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.web.server.ServerHttpSecurity.http
 import org.springframework.security.core.userdetails.User
 import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.security.crypto.factory.PasswordEncoderFactories
